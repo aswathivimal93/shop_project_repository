0fc7d66290a41fb2544aa200f01c5e46d6df0513
aswathivimal93 <86907903+aswathivimal93@users.noreply.github.com>
2024-04-28 16:45:32 +0300

    Menu Functionality and Automatic Amount Calculation for Payments

--- shop/models.py
+++ shop/models.py
@@ -1,6 +1,8 @@
 from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import User,AbstractUser
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 class Shop(models.Model):
@@ -54,8 +56,8 @@ class Payment(models.Model):
     )
 
     amount = models.DecimalField(max_digits=10, decimal_places=2)
-    consumer = models.OneToOneField('Consumer', on_delete=models.CASCADE)
-    shop = models.OneToOneField('Shop', on_delete=models.CASCADE)
+    consumer = models.ForeignKey('Consumer', on_delete=models.CASCADE)
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
     type = models.CharField(max_length=6, choices=AMOUNT_CHOICES, default='credit')
     is_active = models.BooleanField(default=True)
     created_by = models.ForeignKey(User, related_name='payments_created', on_delete=models.CASCADE)
@@ -66,3 +68,46 @@ class Payment(models.Model):
     # Additional fields specific to credit payments
     #credit_limit = models.DecimalField(max_digits=10, decimal_places=2)
     #remaining_credit = models.DecimalField(max_digits=10, decimal_places=2)
+class MenuItem(models.Model):
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE, related_name='menu_items')
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def _str_(self):
+        return self.name
+
+class Order(models.Model):
+    PAYMENT_TYPES = (
+        ('paid', 'Paid'),
+        ('credit', 'Credit'),
+    )
+
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
+    consumer = models.ForeignKey('Consumer', on_delete=models.CASCADE, null=True, blank=True)
+    order_date = models.DateTimeField(auto_now_add=True)
+    payment_type = models.CharField(max_length=10, choices=PAYMENT_TYPES,default='paid')
+    payment = models.ForeignKey('Payment', on_delete=models.CASCADE, null=True, blank=True)
+    total_amount =models.DecimalField(max_digits=10, decimal_places=2,null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if self.payment_type == 'credit' and not self.consumer:
+            raise ValueError("Consumer is required for credit payment.")
+        elif self.payment_type != 'credit':
+            self.consumer = None
+        
+        super().save(*args, **kwargs)
+
+    def update_total_amount(self):
+        total_amount = sum(order_item.subtotal() for order_item in self.order_items.all())
+        self.total_amount = total_amount
+        self.save(update_fields=['total_amount'])
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='order_items')
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+
+    def subtotal(self):
+        return self.menu_item.price *self.quantity
+@receiver(post_save, sender=OrderItem)
+def update_order_total_amount(sender, instance, **kwargs):
+    instance.order.update_total_amount()
--- shop/urls.py
+++ shop/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path,include
 from rest_framework.routers import DefaultRouter
-from shop.api import ShopUserViewSet,ShopViewSet,ConsumerViewSet,PaymentViewSet
+from shop.api import ShopUserViewSet,ShopViewSet,ConsumerViewSet,PaymentViewSet,MenuItemViewSet,OrderViewSet
 
 router = DefaultRouter()
 router.register('api/shopuser', ShopUserViewSet, basename='shopuser')
 router.register('api/shop',ShopViewSet,basename='shop')
 router.register('api/consumer',ConsumerViewSet,basename='consumer')
 router.register('api/payment',PaymentViewSet,basename='payment')
+router.register('api/menuitem',MenuItemViewSet,basename='menuitem')
+router.register('api/order',OrderViewSet,basename='order')
+
+
 
 urlpatterns=router.urls
 
